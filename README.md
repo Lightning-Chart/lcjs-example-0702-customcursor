c762626c85092926848d8b7569497be4770b7b17
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2024-08-05 09:53:07 +0300

    v6.0.0

--- README.md
+++ README.md
@@ -23,28 +23,20 @@ This example serves as an example for creating a custom cursor for XY charts.
 
 Custom cursors can be required for different purposes - like major structural changes or very application specific styling requirements.
 
-If lesser changes to default cursors are required then please see read about different methods of configuring cursor behavior - `ChartXY` API reference has good links and explanations to follow.
+If lesser changes to default cursors are required then please see read about different methods of configuring cursor behavior - Features > Cursor section in Developer documentation is a good reference.
 
-Custom cursors are most importantly based on `LCJS` methods that allow solving nearest data points in series from any supplied location.
-Custom user interactions and data point solving require solid understanding of different coordinate systems in web and `LCJS`, which is the primary reason this example exists;
+All charts expose a simple method: `setCustomCursor` which can be conveniently used to firstly disable the built-in cursor and secondly trigger an automatic callback whenever the cursor should be displayed or hidden, with all information about the pointed data prepared beforehand.
 
-```javascript
-// Add custom action when user moves mouse over series area.
-chart.onSeriesBackgroundMouseMove((_, event) => {
-    // `event` is a native JavaScript event, which packs the active mouse location in `clientX` and `clientY` properties.
-    // it can be used for solving nearest data point ...
-    const nearestDataPoint = series.solveNearestFromScreen(event)
+Thanks to this, regardless how you want to display your custom cursor, it is extremely easy to plug it to LCJS.
 
-    // ... or translated to a pair of Axes
-    const mouseLocationAxis = chart.translateCoordinate(event, chart.coordsAxis)
+```ts
+ChartXY.setCustomCursor((chart, hit, hits) => {
+    // hit : { x, y, series, axisX, axisY, ... }
+    // hits contain information of all cursor hits in case of multi series cursors
 })
 ```
 
-In this example, the custom cursor visual is created with internal `LCJS` user interface components, showcasing a _multi-cursor_ which displays one X coordinate and several Y coordinates at once.
-
-![](./assets/cursor.png)
-
-The result table is created by combining `LCJS` layout elements with `UITextBox` elements - this allows any kind of value grid to be created.
+In this example, the cursor is displayed using UI textboxes and a custom tick on the time axis.
 
 More custom cursor examples can be found by looking for "cursor" tag in the _Interactive Examples_ gallery - for example, using HTML & CSS for an animated result table or connecting to an UI framework.
 
@@ -52,7 +44,6 @@ More custom cursor examples can be found by looking for "cursor" tag in the _Int
 ## API Links
 
 * [Lightning Chart top reference]
-* [Auto cursor modes]
 * [UI element builders]
 * [UI layout builders]
 * [UI backgrounds]
@@ -87,17 +78,16 @@ Direct developer email support can be purchased through a [Support Plan][4] or b
 © LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[Lightning Chart top reference]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/interfaces/LightningChart.html
-[Auto cursor modes]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/enums/AutoCursorModes.html
-[UI element builders]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/variables/UIElementBuilders.html
-[UI layout builders]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/variables/UILayoutBuilders.html
-[UI backgrounds]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/variables/UIBackgrounds.html
-[UI position origin]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/variables/UIOrigins.html
-[Color factory hexadecimal]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/functions/ColorHEX.html
-[Color factory css]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/functions/ColorCSS.html
-[Solid fill style]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/classes/SolidFill.html
-[Solid line style]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/classes/SolidLine.html
-[Chart XY]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/classes/ChartXY.html
-[Axis XY]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/classes/Axis.html
-[Custom tick]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/classes/CustomTick.html
+[Lightning Chart top reference]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/interfaces/LightningChart.html
+[UI element builders]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/variables/UIElementBuilders.html
+[UI layout builders]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/variables/UILayoutBuilders.html
+[UI backgrounds]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/variables/UIBackgrounds.html
+[UI position origin]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/variables/UIOrigins.html
+[Color factory hexadecimal]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/functions/ColorHEX.html
+[Color factory css]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/functions/ColorCSS.html
+[Solid fill style]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/SolidFill.html
+[Solid line style]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/SolidLine.html
+[Chart XY]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/ChartXY.html
+[Axis XY]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/Axis.html
+[Custom tick]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/CustomTick.html
 
--- src/index.js
+++ src/index.js
@@ -5,177 +5,75 @@
  */
 
 // Import LightningChartJS
-const lcjs = require('@arction/lcjs')
-
-// Import data-generators from 'xydata'-library.
-const { createProgressiveTraceGenerator } = require('@arction/xydata')
+const lcjs = require('@lightningchart/lcjs')
 
 // Extract required parts from LightningChartJS.
-const { lightningChart, AutoCursorModes, UIElementBuilders, UILayoutBuilders, UIOrigins, Themes } = lcjs
+const { lightningChart, UIElementBuilders, AxisTickStrategies, UIOrigins, emptyLine, AxisScrollStrategies, emptyFill } = lcjs
 
-// Create a XY Chart.
-const chart = lightningChart({
+const lc = lightningChart({
             resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
         })
+const chart = lc
     .ChartXY({
         theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
     })
-    // Disable native AutoCursor to create custom
-    .setAutoCursorMode(AutoCursorModes.disabled)
-    .setTitle('Custom Cursor using LCJS UI')
+    .setTitle('Custom cursor chart')
 
-// set title for Y axis
-chart.getDefaultAxisY().setTitle('Y-axis')
+const timeAxis = chart
+    .getDefaultAxisX()
+    .setTickStrategy(AxisTickStrategies.Time)
+    .setScrollStrategy(AxisScrollStrategies.progressive)
+    .setDefaultInterval((state) => ({
+        end: state.dataMax ?? 0,
+        start: (state.dataMax ?? 0) - 10_000,
+        stopAxisAfter: false,
+    }))
 
-// generate data and creating the series
-const series = new Array(3).fill(0).map((_, iSeries) => {
-    const nSeries = chart.addLineSeries({
-        dataPattern: {
-            // pattern: 'ProgressiveX' => Each consecutive data point has increased X coordinate.
-            pattern: 'ProgressiveX',
-        },
-    })
-
-    return nSeries
+chart.getDefaultAxisY().dispose()
+const channels = new Array(3).fill(0).map((_, i) => {
+    const yAxis = chart
+        .addAxisY({ iStack: -i })
+        .setTickStrategy(AxisTickStrategies.Empty)
+        .setTitle(`Channel ${i + 1}`)
+        .setTitleRotation(0)
+        .setStrokeStyle(emptyLine)
+        .setAutoRegionsEnabled(false)
+    yAxis.setMargins(2, 2)
+    const series = chart
+        .addPointLineAreaSeries({ yAxis, dataPattern: 'ProgressiveX' })
+        .setAreaFillStyle(emptyFill)
+        .setMaxSampleCount(50_000)
+    return { yAxis, series }
 })
 
-Promise.all(
-    series.map((nSeries) =>
-        createProgressiveTraceGenerator()
-            .setNumberOfPoints(200)
-            .generate()
-            .toPromise()
-            .then((data) => {
-                nSeries.add(data)
-            }),
-    ),
-).then(() => {
-    chart.forEachAxis((axis) => axis.fit(false))
-    requestAnimationFrame(() => {
-        // Show custom cursor at start automatically.
-        showCursorAt({ clientX: window.innerWidth * 0.4, clientY: window.innerHeight / 2 })
-    })
-})
+setInterval(() => {
+    channels.forEach((ch) => ch.series.appendSample({ x: performance.now(), y: Math.random() }))
+}, 1000 / 60)
 
-// Add Legend.
-const legend = chart.addLegendBox().add(chart)
-
-// Create UI elements for custom cursor.
-const resultTable = chart
-    .addUIElement(UILayoutBuilders.Column, {
-        x: chart.getDefaultAxisX(),
-        y: chart.getDefaultAxisY(),
-    })
-    .setMouseInteractions(false)
-    .setOrigin(UIOrigins.LeftBottom)
-    .setMargin(5)
-    .setBackground((background) =>
-        background
-            // Style same as Theme result table.
-            .setFillStyle(chart.getTheme().cursorResultTableFillStyle)
-            .setStrokeStyle(chart.getTheme().cursorResultTableStrokeStyle),
-    )
-
-const rowX = resultTable.addElement(UILayoutBuilders.Row).addElement(UIElementBuilders.TextBox)
-
-const rowsY = series.map((el, i) => {
-    return resultTable
-        .addElement(UILayoutBuilders.Row)
-        .addElement(UIElementBuilders.TextBox)
-        .setTextFillStyle(series[i].getStrokeStyle().getFillStyle())
-})
-
-const tickX = chart.getDefaultAxisX().addCustomTick(UIElementBuilders.PointableTextBox).setAllocatesAxisSpace(false)
-
-const ticksY = series.map((el, i) => {
-    return chart
-        .getDefaultAxisY()
-        .addCustomTick(UIElementBuilders.PointableTextBox)
-        .setAllocatesAxisSpace(false)
-        .setMarker((marker) => marker.setTextFillStyle(series[i].getStrokeStyle().getFillStyle()))
-})
-
-// Hide custom cursor components initially.
-resultTable.setVisible(false)
-tickX.setVisible(false)
-ticksY.forEach((tick) => tick.setVisible(false))
-
-const showCursorAt = (clientCoordinate) => {
-    // Translate mouse location to Axis.
-    const mouseLocationAxis = chart.translateCoordinate(clientCoordinate, chart.coordsAxis)
-
-    // Solve nearest data point to the mouse on each series.
-    const nearestDataPoints = series.map((el) => el.solveNearestFromScreen(clientCoordinate))
-
-    // Find the nearest solved data point to the mouse.
-    const nearestPoint = nearestDataPoints.reduce((prev, curr, i) => {
-        if (!prev) return curr
-        if (!curr) return prev
-        return Math.abs(mouseLocationAxis.y - curr.location.y) < Math.abs(mouseLocationAxis.y - prev.location.y) ? curr : prev
-    })
-
-    if (nearestPoint) {
-        // Set custom cursor location.
-        resultTable.setPosition({
-            x: nearestPoint.location.x,
-            y: nearestPoint.location.y,
-        })
-
-        // Change origin of result table based on cursor location.
-        if (nearestPoint.location.x > chart.getDefaultAxisX().getInterval().end / 1.5) {
-            if (nearestPoint.location.y > chart.getDefaultAxisY().getInterval().end / 1.5) {
-                resultTable.setOrigin(UIOrigins.RightTop)
-            } else {
-                resultTable.setOrigin(UIOrigins.RightBottom)
-            }
-        } else if (nearestPoint.location.y > chart.getDefaultAxisY().getInterval().end / 1.5) {
-            resultTable.setOrigin(UIOrigins.LeftTop)
-        } else {
-            resultTable.setOrigin(UIOrigins.LeftBottom)
-        }
-
-        // Format result table text.
-        rowX.setText(`X: ${chart.getDefaultAxisX().formatValue(nearestPoint.location.x)}`)
-        rowsY.map((rowY, i) => {
-            rowY.setText(`Y${i}: ${chart.getDefaultAxisY().formatValue(nearestDataPoints[i].location.y)}`)
-        })
-
-        // Position custom ticks.
-        tickX.setValue(nearestPoint.location.x)
-        ticksY.forEach((tick, i) => {
-            tick.setValue(nearestDataPoints[i].location.y || 0)
-        })
-
-        // Display cursor.
-        resultTable.setVisible(true)
-        tickX.setVisible(true)
-        ticksY.map((el) => el.setVisible(true))
-    } else {
-        // Hide cursor.
-        resultTable.setVisible(false)
-        tickX.setVisible(false)
-        ticksY.map((el) => el.setVisible(false))
+// ::: Custom cursor with values displayed in top-left corner of each channel :::
+const cursorValueDisplays = channels.map((ch) =>
+    chart
+        .addUIElement(UIElementBuilders.CheckBox, { x: timeAxis, y: ch.yAxis })
+        .setVisible(false)
+        .setMargin(0)
+        .setOn(true)
+        .setMouseInteractions(false),
+)
+const cursorTimeTick = timeAxis.addCustomTick(UIElementBuilders.PointableTextBox).setVisible(false)
+chart.setCustomCursor((_, __, hits) => {
+    if (!hits) {
+        cursorValueDisplays.forEach((ui) => ui.setVisible(false))
+        cursorTimeTick.setVisible(false)
+        return
     }
-}
-
-// Implement custom cursor logic with events.
-const mouseMoveEventHandler = (_, event) => {
-    showCursorAt(event)
-}
-
-chart.onSeriesBackgroundMouseMove(mouseMoveEventHandler)
-series.forEach((series) => {
-    series.onMouseMove(mouseMoveEventHandler)
-})
-
-chart.onSeriesBackgroundMouseLeave((_, e) => {
-    resultTable.setVisible(false)
-    tickX.setVisible(false)
-    ticksY.map((el) => el.setVisible(false))
-})
-
-chart.onSeriesBackgroundMouseDragStart((_, e) => {
-    resultTable.setVisible(false)
-    tickX.setVisible(false)
-    ticksY.map((el) => el.setVisible(false))
+    hits.forEach((hit) => {
+        const iCh = channels.findIndex((item) => item.series === hit.series)
+        const ui = cursorValueDisplays[iCh]
+        ui.setVisible(true)
+            .setText(`${hit.series.getName()} ${hit.axisY.formatValue(hit.y)}`)
+            .setPosition({ x: timeAxis.getInterval().start, y: hit.axisY.getInterval().end })
+            .setOrigin(UIOrigins.LeftTop)
+        hit.series.styleLegendEntry(ui)
+    })
+    cursorTimeTick.setVisible(true).setValue(hits[0].x)
 })
